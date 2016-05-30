d34782df1602ac1825505d392dfc59af51906fd3
Lauri Hiltunen <lauri.hiltunen@ambientia.fi>
2016-05-30 21:43:23 +0300

    Below low value test

--- Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
+++ Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
@@ -1,6 +1,6 @@
 package tddmicroexercises.tirepressuremonitoringsystem;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 
 import org.junit.Test;
@@ -8,7 +8,8 @@ import org.mockito.Mockito;
 
 public class TestAlarm {
 
-    @Test
+    
+	@Test
     public void shouldBeOffAsDefault() {
         Alarm alarm = new Alarm();
         assertEquals(false, alarm.isAlarmOn());
@@ -24,5 +25,15 @@ public class TestAlarm {
 		alarm.check();
 		assertEquals(true, alarm.isAlarmOn());
 		
+	}
+    
+    @Test
+	public void shouldBeOnWhenValueBelowTheLowPressureTreshold() throws Exception {
+		Sensor sensor = mock(Sensor.class);
+		Mockito.when(sensor.popNextPressurePsiValue()).thenReturn(1d);
+		
+		Alarm alarm = new Alarm(sensor);
+		alarm.check();
+		assertEquals(true, alarm.isAlarmOn());
 	}
 }
