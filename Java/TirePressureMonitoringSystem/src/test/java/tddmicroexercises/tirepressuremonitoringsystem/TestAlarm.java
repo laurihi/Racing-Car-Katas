e62f7b97020a001165a7f122be2d41de3e880c87
Lauri Hiltunen <lauri.hiltunen@ambientia.fi>
2016-05-30 21:48:15 +0300

    Test the limits

--- Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
+++ Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/TestAlarm.java
@@ -3,25 +3,31 @@ package tddmicroexercises.tirepressuremonitoringsystem;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 public class TestAlarm {
 
     
+	private Sensor sensor;
+	private Alarm alarm;
+
+	@Before
+	public void setUp(){
+		sensor = mock(Sensor.class);
+		alarm = new Alarm(sensor);
+	}
+	
 	@Test
     public void shouldBeOffAsDefault() {
-        Alarm alarm = new Alarm();
         assertEquals(false, alarm.isAlarmOn());
     }
     
     @Test
 	public void shouldBeOnWhenValueAboveTheHighPressureTreshold() throws Exception {
 		
-    	Sensor sensor = mock(Sensor.class);
 		Mockito.when(sensor.popNextPressurePsiValue()).thenReturn(22d);
-		
-		Alarm alarm = new Alarm(sensor);
 		alarm.check();
 		assertEquals(true, alarm.isAlarmOn());
 		
@@ -29,11 +35,22 @@ public class TestAlarm {
     
     @Test
 	public void shouldBeOnWhenValueBelowTheLowPressureTreshold() throws Exception {
-		Sensor sensor = mock(Sensor.class);
 		Mockito.when(sensor.popNextPressurePsiValue()).thenReturn(1d);
-		
-		Alarm alarm = new Alarm(sensor);
 		alarm.check();
 		assertEquals(true, alarm.isAlarmOn());
+	}
+    
+    @Test
+	public void shouldBeOffWhenAtTheLowPressureTreshold() throws Exception {
+    	Mockito.when(sensor.popNextPressurePsiValue()).thenReturn(17d);
+		alarm.check();
+		assertEquals(false, alarm.isAlarmOn());
+	}
+    
+    @Test
+	public void shouldBeOffWhenAtTheHighPressureTreshold() throws Exception {
+    	Mockito.when(sensor.popNextPressurePsiValue()).thenReturn(21d);
+		alarm.check();
+		assertEquals(false, alarm.isAlarmOn());
 	}
 }
